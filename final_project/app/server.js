56283ff08f2425ccbe4c88d364d57388e474335f
antonioalexandru <60381460+antonioalexandru@users.noreply.github.com>
2020-03-05 12:35:29 +0200

    Update server.js

--- final_project/app/server.js
+++ final_project/app/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: "antoniosql-nfs-antonio.9.212.159.114.nip.io:3306",
+ host: "antoniosql-nfs-antonio.9.212.159.114.nip.io",
  user: "root",
  password: "test",
  database: "test"
